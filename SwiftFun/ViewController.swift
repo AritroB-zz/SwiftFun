3286224ee9a8037542d6b6b5aa1304ee784d478f
AritroB <abasuusa2010@gmail.com>
2020-07-14 18:40:47 -0500

    Initial Commit

---
+++ SwiftFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFun
+//
+//  Created by Aritro Basu on 7/14/20.
+//  Copyright © 2020 Aritro Basu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
